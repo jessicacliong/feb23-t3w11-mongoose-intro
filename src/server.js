d92e08bdf7a7d0aa3b20434272fee6885bd36328
jessicacliong <14395@coderacademy.edu.au>
2023-10-31 19:53:46 +1100

    feat: server hello world implemented

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+// make a server instance
+const app = express();
+
+app.get("/", (request, response) => {
+     response.json({
+          message:"Hello world!"
+     });
+});
+
+module.exports = {
+     app
+}
